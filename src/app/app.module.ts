510016e1f18282d1feaf49c3535ca30c2f375396
PedroNeves-git <sgtpedrocos@gmail.com>
2025-02-25 19:39:51 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './componentes/card/card.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HeaderComponent } from './componentes/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
